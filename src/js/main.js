35377affb4afd1ee856e7435f47aee52668641f4
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-06 23:59:24 +0300

    added final project

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+  $('.works__item-btn').on('click', function(){
+    $(this).toggleClass('works__item-btn--active');
+  });
+  
+  $('.works__item-btn').click(function(){
+    if (!$(this).data('status')) {
+      $(this).html('სამსახურის დატოვება');
+      $(this).data('status', true);
+    }
+    else {
+      $(this).html('დაწყება');
+      $(this).data('status', false);
+    }
+  });
+
+
 });
